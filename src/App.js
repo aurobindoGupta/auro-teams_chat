51d3ce1fd0405cb97b91089da71a531bfac12918
AurobindoGupta <aurogpt10@gmail.com>
2021-05-07 14:50:27 +0530

    started

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
-import { ChatEngine } from 'react-chat-engine'
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import './App.css';
+
+
+
+const App = () => {
+
+    return(
+        <ChatEngine
+            height= "100vh"
+            projectID = "74869fe0-0cb7-4eff-bfb7-b47888babb20"
+            userName="auro"
+            userSecret= "1234"
+            renderChatFeed = {(chatAppProps) => <ChatFeed { ...chatAppProps} /> } 
+        />
+    );
+}
+
+
+export default App;
+
---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,27 @@
+import MessageForm from './MessageForm';
+import MyMessage from './MyMessage';
+import TheirMessage from './TheirMessage';
+
+const Chatfeed = (props) => {
+
+        const { chats, activeChat, userName, messages } = props;
+
+        const chat = chats && chats[activeChat];
+
+        const renderMessages = () => {
+
+            const keys = Object.keys(messages);
+
+            // console.log(keys);
+        }
+        renderMessages();
+
+        return(
+
+            <div>
+                ChatFeed
+            </div>
+        );
+}
+
+export default Chatfeed;
